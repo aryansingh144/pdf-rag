bbde1a18bcc062c79cdc28a596446418bc1d4312
your-username <your-email@example.com>
2025-06-03 02:31:17 +0530

    Initial commit for pdf-rag

---
+++ client/app/components/file-upload.tsx
@@ -0,0 +1,33 @@
+'use client'
+import React from 'react';
+import {Upload} from 'lucide-react';
+
+const handleFileUploadButtonClick = () => {
+    const el=document.createElement('input');
+    el.setAttribute('type', 'file');
+    el.setAttribute('accept', 'application/pdf');
+    el.addEventListener('change', (event) => {
+        if(el.files && el.files.length > 0) {
+            const  file=el.files[0];
+        }
+    })
+    el.click();
+}
+
+const FileUploadComponent: React.FC = () => {
+    return (
+        <div className='bg-slate-900 text-white shadow-2xl flex justify-center items-center p-4 rounded-lg border-2 border-white'>
+            <div 
+            onClick={handleFileUploadButtonClick} 
+            className='flex justify-center items-center flex-col cursor-pointer'>
+
+                <h3 >Upload PDF file </h3>
+                <Upload />
+
+            </div>
+        </div>
+    )
+
+}
+
+export default FileUploadComponent;
