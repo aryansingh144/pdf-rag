c27eeb09150434b18c2d5c279e81d8d89d0e8fc1
your-username <aryansingh7910@gmail.com>
2025-06-07 17:56:54 +0530

    multer and file upload done

--- server/index.js
+++ server/index.js
@@ -1,13 +1,31 @@
 import express from 'express';
 import cors from 'cors';
 import multer from 'multer';
+import path from 'path';
+import fs from 'fs';
+
+// const uploadDir = path.join(process.cwd(), 'uploads');
+// if (!fs.existsSync(uploadDir)) {
+//   fs.mkdirSync(uploadDir);
+// }
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9)
+    cb(null, `${uniqueSuffix}-${file.originalname}`)
+  }
+})
+
+const upload =multer({
+storage: storage
+});
 
 const app = express();
 app.use(cors());
 
- const upload =multer({
-  dest: 'uploads/'
-});
 
 
 app.get('/', (req, res) => {
@@ -15,7 +33,7 @@ app.get('/', (req, res) => {
 }
 );
 
-app.post('/photos/upload', upload.single('pdf'), (req, res) => {
+app.post('/upload/pdf', upload.single('pdf'), (req, res) => {
   if (!req.file) {
     return res.status(400).json({ error: 'No file uploaded'});
   }
